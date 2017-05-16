617ed1bb25129ad9e0c7270999d5efd394323ee0
Sayantan Saha <Sayantan Saha>
2017-05-16 10:22:13 +0530

    I added a new servo arduino code

---
+++ servo.ino
@@ -0,0 +1,44 @@
+#include <SoftwareSerial.h>
+
+#include <Servo.h>
+
+Servo myservo;
+
+int bluetoothTx = 10;
+
+int bluetoothRx = 11;
+
+SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);
+
+void setup()
+
+{
+
+myservo.attach(9);
+
+
+Serial.begin(9600);
+
+
+bluetooth.begin(9600);
+
+}
+
+void loop()
+
+{
+
+
+if(bluetooth.available()> 0 )
+
+{
+
+int servopos = bluetooth.read();
+
+Serial.println(servopos);
+
+myservo.write(servopos);
+
+}
+
+}
